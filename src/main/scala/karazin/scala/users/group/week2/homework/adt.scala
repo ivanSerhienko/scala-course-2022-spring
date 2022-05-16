fbb444b79a0f5c11aa11164dbf854768051f3e87
ivanSerhienko <ivan.serhienko.2001@gmail.com>
2022-05-16 15:47:37 +0300

    Homework number 2 without some functions.

--- src/main/scala/karazin/scala/users/group/week2/homework/adt.scala
+++ src/main/scala/karazin/scala/users/group/week2/homework/adt.scala
@@ -9,66 +9,55 @@ package karazin.scala.users.group.week2.homework
   * https://docs.scala-lang.org/scala3/book/types-adts-gadts.html
 */
 
-object adt:
-  
-  enum ErrorOr[+V]:
-    
-    // Added to make it compilable. Remove it.
-    case DummyCase
-    
-    /* 
-      Two case must be defined: 
-      * a case for a regular value
-      * a case for an error (it should contain an actual throwable)
-     */
-  
-    /* 
-      The method is used for defining execution pipelines
-      Provide a type parameter, an argument and a result type
-      
-      Make sure that in case of failing the method with exception
-      no exception is thrown but the case for an error is returned
-    */ 
-    def flatMap = ???
 
-    /* 
-      The method is used for changing the internal object
-      Provide a type parameter, an argument and a result type
-      
-      Make sure that in case of failing the method with exception
-      no exception is thrown but the case for an error is returned
-     */
-    def map = ???
-  
-    /* 
-      The method is used for filtering
-      Provide a type parameter, an argument and a result type
-      
-      Make sure that in case of failing the method with exception
-      no exception is thrown but the case for an error is returned
-     */
-    def withFilter = ???
-  
-    /* 
-      The method is used for getting rid of internal box
-      Provide a type parameter, an argument and a result type
-    */
-    def flatten = ???
-    
-    /* 
-      The method is used for applying side effects without returning any result
-      Provide a type parameter, an argument and a result type
-    */
-    def foreach = ???
-      
+object adt:
+
+  enum ErrorOr[+V]:
+
+    case Or(x: V) extends ErrorOr[V]
+    case Error(ex: Throwable) extends ErrorOr[V]
+
+    // The method is used for defining execution pipelines
+    def flatMap[Q](f: V => ErrorOr[Q]): ErrorOr[Q] =
+      this match
+        case ErrorOr.Error(ex)  => ErrorOr.Error(ex)
+        case ErrorOr.Or(x)      => try f(x) catch {
+          case ex: Throwable    => ErrorOr.Error(ex)
+        }
+
+    //The method is used for changing the internal object
+    def map[Q](f: V => Q): ErrorOr[Q] =
+      this match
+        case ErrorOr.Error(ex) => ErrorOr.Error(ex)
+        case ErrorOr.Or(x)     => try ErrorOr.Or(f(x)) catch {
+          case ex: Throwable   => ErrorOr.Error(ex)
+        }
+
+    //The method is used for filtering
+    def withFilter(p: V => Boolean): ErrorOr[V] =
+      this match 
+        case ErrorOr.Error(ex) => ErrorOr.Error(ex)
+        case ErrorOr.Or(x) if p(x) => ErrorOr.Or(x)
+
+    //The method is used for getting rid of internal box
+    def flatten[U](implicit ev: V <:< ErrorOr[U]): ErrorOr[U] = 
+      this match 
+        case ErrorOr.Error(ex) => ErrorOr.Error(ex)
+        case ErrorOr.Or(x)     => try ev(x) catch {
+          case ex: Throwable   => ErrorOr.Error(ex)
+        }
+
+    //The method is used for applying side effects without returning any result
+    def foreach[U](f: V => U): Unit =
+      this match 
+        case ErrorOr.Error(ex) => ErrorOr.Error(ex)
+        case ErrorOr.Or(x)     => try f(x) catch {
+        case ex: Throwable   => ErrorOr.Error(ex)
+      }
+
   // Companion object to define constructor
   object ErrorOr:
-    /* 
-      Provide a type parameter, an argument and a result type
-      
-      Make sure that in case of failing the method with exception
-      no exception is thrown but the case for an error is returned
-    */
-    def apply = ???
-      
-  
+    def apply[V](v: => V): ErrorOr[V] =
+      try ErrorOr.Or(v) catch {
+        case ex: Throwable => ErrorOr.Error(ex)
+      }
--- src/main/scala/karazin/scala/users/group/week2/homework/program.scala
+++ src/main/scala/karazin/scala/users/group/week2/homework/program.scala
@@ -5,29 +5,48 @@ import adt._
 import model._
 import services._
 
-object program:
-  
-  /*
-   Print all view for all user's posts if they exists
-  */
+object program extends App:
+
+
+  // Not work
+  //Getting view for all user's posts if they exists
+/*  def getPostsViews(): ErrorOr[List[PostView]] =
+    for
+      profile   ← getUserProfile()
+      posts     ← getPosts(profile.userId)
+      postsView ← getPostView(ErrorOr(posts))
+      if postsView.length == 42
+    yield postsView*/
+
+  //Print all view for all user's posts if they exists
   def printPostsViews(): ErrorOr[List[PostView]] = ???
- 
-  
-  /*
-   Getting view for all user's posts if they exists
-  */
-  def getPostsViews(): ErrorOr[List[PostView]] = ???
 
-  /* 
-    Getting view for a particular user's post
-    Provide an argument and a result type
-  */
-  def getPostView(post: Post) = ???
 
-  /*
-   Provide desugared version of the previous two methods
-  */
-  def getPostsViewDesugared() = ???
+  //Getting view for a particular user's post
+  def getPostView(post: Post) =
+    for
+      comments  ← getComments(post.postId)
+      likes     ← getLikes(post.postId)
+      shares    ← getShares(post.postId)
+    yield PostView(post, comments, likes, shares)
+
+  //Provide desugared version of the previous two methods
+  def getPostsViewDesugared(): ErrorOr[List[ErrorOr[PostView]]] =
+    getUserProfile() flatMap { profile ⇒
+      getPosts(profile.userId)
+    } map { posts ⇒
+      posts map { post ⇒
+        getComments(post.postId) flatMap { comments ⇒
+          getLikes(post.postId) flatMap { likes ⇒
+            getShares(post.postId) map { shares ⇒
+              PostView(post, comments, likes, shares)
+            }
+          }
+        }
+      }
+    } withFilter { postsView ⇒
+      postsView.length == 42
+    }
 
 
   
